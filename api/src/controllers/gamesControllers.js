c78254fd049f4fefd05a3798c0f55fda6a498e88
unknown <shaicokohn@gmail.com>
2024-08-26 22:59:41 -0300

    Controllers updated to work with the database

--- api/src/controllers/gamesControllers.js
+++ api/src/controllers/gamesControllers.js
@@ -1,29 +1,25 @@
 const { default: axios } = require("axios")
 const { API_URL } = process.env
+const Pokemon = require("../models/pokemon")
 
 const getPokemonsForWhoIsThatPokemon = async(req, res) => {
     try {
-        let pokemons = await(axios.get(`${API_URL}/pokemon/?limit=100`))
+        let pokemons = await Pokemon.find({})
         
-        let pokemonsData = pokemons.data.results
-        let pokemonFilteredData = []
-        for(let i = 0; i < pokemonsData.length; i++) {
-            let pokemonForm = await(axios.get(`${API_URL}/pokemon-form/${pokemonsData[i].name}`))
-            let pokemonSprite = pokemonForm.data.sprites.front_default
-            pokemonsData[i] = {
-                name: pokemonsData[i].name,
-                image: pokemonSprite,
+        for(let i = 0; i < pokemons.length; i++) {
+            pokemons[i] = {
+                name: pokemons[i].name,
+                image: pokemons[i].image,
             }
-            pokemonFilteredData.push(pokemonsData[i])
         }
 
         function shuffleArray(q){
             q.sort(()=> Math.random() - 0.5);
         }
 
-        shuffleArray(pokemonsData)
+        shuffleArray(pokemons)
 
-        res.status(200).json(pokemonsData)
+        res.status(200).json(pokemons)
     } catch(e) {
         res.status(500).json({msg: String(e)})
     }
@@ -31,30 +27,25 @@ const getPokemonsForWhoIsThatPokemon = async(req, res) => {
 
 const getPokemonsForHigherOrLower = async(req, res) => {
     try {
-        let pokemons = await(axios.get(`${API_URL}/pokemon/?limit=50`))
-        
-        let pokemonsData = pokemons.data.results
-        let pokemonFilteredData = []
-        for(let i = 0; i < pokemonsData.length; i++) {
-            let pokemonForm = await(axios.get(`${API_URL}/pokemon-form/${pokemonsData[i].name}`))
-            let pokemonSprite = pokemonForm.data.sprites.front_default
-            pokemonsData[i] = {
-                name: pokemonsData[i].name,
-                image: pokemonSprite,
-                weight: pokemonsData[i].weight / 10,
-                height: pokemonsData[i].height / 10,
-                id: pokemonsData[i].id,
+        let pokemons = await Pokemon.find({})
+
+        for(let i = 0; i < pokemons.length; i++) {
+            pokemons[i] = {
+                name: pokemons[i].name,
+                image: pokemons[i].image,
+                weight: pokemons[i].weight / 10,
+                height: pokemons[i].height / 10,
+                id: pokemons[i].id,
             }
-            pokemonFilteredData.push(pokemonsData[i])
         }
 
         function shuffleArray(q){
             q.sort(()=> Math.random() - 0.5);
         }
 
-        shuffleArray(pokemonsData)
+        shuffleArray(pokemons)
 
-        res.status(200).json(pokemonsData)
+        res.status(200).json(pokemons)
     } catch(e) {
         res.status(500).json({msg: String(e)})
     }
--- api/src/controllers/pokemonsControllers.js
+++ api/src/controllers/pokemonsControllers.js
@@ -38,20 +38,15 @@ const getPokemonsToDatabase = async(req, res) => {
 
 const getPokemons = async(req, res) => {
     try {
-        let pokemons = await(axios.get(`${API_URL}/pokemon/?limit=40`))
-        
-        let pokemonsData = pokemons.data.results
-        let pokemonFilteredData = []
-        for(let i = 0; i < pokemonsData.length; i++) {
-            let pokemonForm = await(axios.get(`${API_URL}/pokemon-form/${pokemonsData[i].name}`))
-            let pokemonSprite = pokemonForm.data.sprites.front_default
-            pokemonsData[i] = {
-                name: pokemonsData[i].name,
-                image: pokemonSprite,
+        let pokemons = await Pokemon.find({})
+
+        for(let i = 0; i < pokemons.length; i++) {
+            pokemons[i] = {
+                name: pokemons[i].name,
+                image: pokemons[i].image,
             }
-            pokemonFilteredData.push(pokemonsData[i])
         }
-        res.status(200).json(pokemonFilteredData)
+        res.status(200).json(pokemons)
     } catch(e) {
         res.status(500).json({msg: String(e)})
     }
@@ -60,33 +55,8 @@ const getPokemons = async(req, res) => {
 const getPokemonByName = async(req, res) => {
     const { name } = req.params
     try {
-        let pokemon = await (axios.get(`${API_URL}/pokemon/${name}`)) 
-        let abilities = pokemon.data.abilities
-        let types = pokemon.data.types
-        let image
-
-        for(let i = 0; i < abilities.length; i++) {
-            let ability = await (axios.get(`${abilities[i].ability.url}`))
-            let abilityInEnglish = ability.data.effect_entries.find((l) => l.language.name === "en")
-            abilities[i] = {
-                name: abilities[i].ability.name,
-                effect: abilityInEnglish.short_effect,
-            }
-        }
-
-        for(let i = 0; i < types.length; i++) {
-            types[i] = types[i].type.name
-        }
-
-        let pokemonInfo = {
-            name: pokemon.data.name,
-            image,
-            types,
-            weight: pokemon.data.weight / 10,
-            height: pokemon.data.height / 10,
-            abilities,
-        }
-        res.status(200).json(pokemonInfo)
+        let pokemon = await Pokemon.find({name: { $in: name }}) 
+        res.status(200).json(pokemon)
     } catch(e) {
         res.status(500).json({msg: String(e)})
     }
