4b3553b1d4504ea595052618be407a408c3b4a75
unknown <shaicokohn@gmail.com>
2024-09-02 21:06:59 -0300

    Responsive NavBar

---
+++ client/src/components/NavBar/MobileNavbar.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import './MobileNavBar.css'
+import { Link } from 'react-router-dom'
+import { RxHamburgerMenu } from "react-icons/rx"
+import { RiCloseFill} from "react-icons/ri"
+
+const MobileNavbar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className='mobileNavContainer'>
+            {
+                !isOpen ?
+                <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                    <h2 style={{textAlign: 'center'}}>POKESHAI</h2>
+                    <button className="mobileNavButtons" type="button" onClick={toggleMenu}>
+                        <RxHamburgerMenu color="white" size={40} />
+                    </button>
+                </div>
+                :
+                <div style={{display: 'flex', justifyContent: 'space-around'}}>
+                    <h2>POKESHAI</h2>
+                    <button className="mobileNavButtons" type="button" onClick={toggleMenu}>
+                        <RiCloseFill color="white" size={40} />
+                    </button>
+                </div>
+            }
+            {
+                isOpen ? 
+                <div style={{textAlign: 'center'}}>
+                    <Link className='mobileLinks' to='/'>HOME</Link>
+                    <br />
+                    <Link className='mobileLinks' to='/pokemons'>POKEMONS</Link>
+                    <br />
+                    <Link className='mobileLinks' to='/whoIsThatPokemon'>WHO IS THAT POKEMON?</Link>
+                    <br />
+                    <Link className='mobileLinks' to='/higherOrLower'>HIGHER OR LOWER?</Link>
+                </div> : ''
+            }
+        </div>
+    )
+}
+
+export default MobileNavbar
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import './NavBar.css'
 import { Link } from 'react-router-dom'
+import MobileNavbar from './MobileNavbar'
 
 const NavBar = () => {
   return (
-    <div className='navContainer'>
-      <Link className='links' to='/'>HOME</Link>
+    <div>
+      <div className='mobile-only'>
+        <MobileNavbar />
+      </div>
+      <div className='navContainer navBar-pc'>
+        <Link className='links' to='/'>HOME</Link>
         <Link className='links' to='/pokemons'>POKEMONS</Link>
         <Link className='links' to='/whoIsThatPokemon'>WHO IS THAT POKEMON?</Link>
         <Link className='links' to='/higherOrLower'>HIGHER OR LOWER?</Link>
+      </div>
     </div>
   )
 }
