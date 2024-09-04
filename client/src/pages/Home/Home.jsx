af6015b689b53c27a0c8c79b68e61814a7466317
unknown <shaicokohn@gmail.com>
2024-09-03 21:30:47 -0300

    Responsive Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,22 +1,34 @@
-import NavBar from '../../components/NavBar/NavBar'
+import { Link } from 'react-router-dom'
 import './Home.css'
+import MobileHome from './MobileHome'
 
 const Home = () => {
   return (
-    <div className='homeContainer'>
-        <h1 style={{color: 'red'}}>Shai's PokeProject</h1>
-        <div className='titlesContainer'>
+    <div>
+      <div className='mobile-only'>
+        <MobileHome />
+      </div>
+      <div className='homeContainer pc-only'>
+        <h1 style={{color: 'red'}}>POKESHAI</h1>
+        <Link to='/pokemons'>
+          <div className='titlesContainer'>
             <h1>POKEMONS</h1>
-            <p className='description'>Here you can see INFO about all the pokemons!</p>
-        </div>
-        <div className='titlesContainer'>
+            <p className='description'>Here you can search all the pokemons!</p>
+          </div>
+        </Link>
+        <Link to='/whoIsThatPokemon'>
+          <div className='titlesContainer'>
             <h1>WHO IS THAT POKEMON?</h1>
-            <p className='description'>Here you can play the famous game of the anime</p>
-        </div>
-        <div className='titlesContainer'>
+            <p className='description'>Here you can play the famous game of the anime!</p>
+          </div>
+        </Link>
+        <Link to='/higherOrLower'>
+          <div className='titlesContainer'>
             <h1>HIGHER OR LOWER?</h1>
-            <p className='description'>Here you can play the famous game of Higher Or Lower adapted to the Pokemon World!</p>
-        </div>
+            <p className='description'>Here you can play Higher Or Lower adapted to Pokemon!</p>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
--- client/src/pages/Home/MobileHome.jsx
+++ client/src/pages/Home/MobileHome.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import'./MobileHome.css'
 
 const MobileHome = () => {
   return (
-    <div>MobileHome</div>
+    <div className='mobileHomeContainer'>
+        <h1 style={{color: 'red'}}>POKESHAI</h1>
+        <Link to='/pokemons'>
+          <div className='mobileTitlesContainer'>
+            <h1 style={{fontSize: '2.5em'}}>POKEMONS</h1>
+            <p className='mobileDescription'>Here you can search all the pokemons!</p>
+          </div>
+        </Link>
+        <Link to='/whoIsThatPokemon'>
+          <div className='mobileTitlesContainer'>
+            <h1 style={{fontSize: '2.5em'}}>WHO IS THAT POKEMON?</h1>
+            <p className='mobileDescription'>Here you can play the famous game of the anime!</p>
+          </div>
+        </Link>
+        <Link to='/higherOrLower'>
+          <div className='mobileTitlesContainer'>
+            <h1 style={{fontSize: '2.5em'}}>HIGHER OR LOWER?</h1>
+            <p className='mobileDescription'>Here you can play Higher Or Lower adapted to Pokemon!</p>
+          </div>
+        </Link>
+      </div>
   )
 }
 
